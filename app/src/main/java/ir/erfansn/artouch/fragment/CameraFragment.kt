00ab8e652789bfb2ac81f04fe265b46f1e733f71
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-04-29 22:41:34 +0330

    Implement CameraX Preview use case

--- app/src/main/java/ir/erfansn/artouch/fragment/CameraFragment.kt
+++ app/src/main/java/ir/erfansn/artouch/fragment/CameraFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.concurrent.futures.await
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import ir.erfansn.artouch.R
 import ir.erfansn.artouch.databinding.FragmentCameraBinding
 import ir.erfansn.artouch.fragment.PermissionsFragment.Companion.isCameraPermissionGranted
+import kotlinx.coroutines.launch
 
 class CameraFragment : Fragment() {
 
@@ -22,6 +28,26 @@ class CameraFragment : Fragment() {
     ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startCamera()
+    }
+
+    private fun startCamera() {
+        lifecycleScope.launch {
+            val cameraProvider = ProcessCameraProvider.getInstance(requireActivity()).await()
+
+            val preview = Preview.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(requireActivity(), cameraSelector, preview)
+
+            preview.setSurfaceProvider(binding.preview.surfaceProvider)
+        }
     }
 
     override fun onResume() {
