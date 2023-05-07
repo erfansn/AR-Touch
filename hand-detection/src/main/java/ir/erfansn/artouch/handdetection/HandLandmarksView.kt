d4cd2c232f9d82159e12266d76c7caf86e6fa179
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-05-07 12:07:57 +0330

    Add HandLandmarksView

---
+++ hand-detection/src/main/java/ir/erfansn/artouch/handdetection/HandLandmarksView.kt
@@ -0,0 +1,131 @@
+package ir.erfansn.artouch.handdetection
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmark
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarkerResult
+import kotlin.math.max
+
+class HandLandmarksView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private var result: HandLandmarkerResult? = null
+    private var linePaint = Paint()
+    private var pointPaint = Paint()
+
+    private var scaleFactor: Float = 1f
+    private var imageWidth: Int = 1
+    private var imageHeight: Int = 1
+
+    init {
+        initPaints()
+    }
+
+    private fun initPaints() {
+        linePaint.color = ContextCompat.getColor(context!!, android.R.color.darker_gray)
+        linePaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        linePaint.style = Paint.Style.STROKE
+
+        pointPaint.color = Color.YELLOW
+        pointPaint.strokeWidth = LANDMARK_STROKE_WIDTH
+        pointPaint.style = Paint.Style.FILL
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        result?.let { handLandmarkerResult ->
+            val lines = mutableListOf<Float>()
+            val points = mutableListOf<Float>()
+
+            for (landmarks in handLandmarkerResult.landmarks()) {
+                for (i in landmarkConnections.indices step 2) {
+                    val startX =
+                        landmarks[landmarkConnections[i]].x() * imageWidth * scaleFactor - (imageWidth * scaleFactor - width).toInt() / 2
+                    val startY =
+                        landmarks[landmarkConnections[i]].y() * imageHeight * scaleFactor - (imageHeight * scaleFactor - height).toInt() / 2
+                    val endX =
+                        landmarks[landmarkConnections[i + 1]].x() * imageWidth * scaleFactor - (imageWidth * scaleFactor - width).toInt() / 2
+                    val endY =
+                        landmarks[landmarkConnections[i + 1]].y() * imageHeight * scaleFactor - (imageHeight * scaleFactor - height).toInt() / 2
+                    lines.add(startX)
+                    lines.add(startY)
+                    lines.add(endX)
+                    lines.add(endY)
+                    points.add(startX)
+                    points.add(startY)
+                }
+                canvas.drawLines(lines.toFloatArray(), linePaint)
+                canvas.drawPoints(points.toFloatArray(), pointPaint)
+            }
+        }
+    }
+
+    fun setResult(
+        result: HandDetectionResult,
+    ) {
+        this.result = result.landmarks
+
+        this.imageHeight = result.inputImageHeight
+        this.imageWidth = result.inputImageWidth
+
+        scaleFactor = max(width / imageWidth.toFloat(), height / imageHeight.toFloat())
+
+        invalidate()
+    }
+
+    companion object {
+        private const val LANDMARK_STROKE_WIDTH = 8F
+
+        // This list defines the lines that are drawn when visualizing the hand landmark detection
+        // results. These lines connect:
+        // landmarkConnections[2*n] and landmarkConnections[2*n+1]
+        private val landmarkConnections = listOf(
+            HandLandmark.WRIST,
+            HandLandmark.THUMB_CMC,
+            HandLandmark.THUMB_CMC,
+            HandLandmark.THUMB_MCP,
+            HandLandmark.THUMB_MCP,
+            HandLandmark.THUMB_IP,
+            HandLandmark.THUMB_IP,
+            HandLandmark.THUMB_TIP,
+            HandLandmark.WRIST,
+            HandLandmark.INDEX_FINGER_MCP,
+            HandLandmark.INDEX_FINGER_MCP,
+            HandLandmark.INDEX_FINGER_PIP,
+            HandLandmark.INDEX_FINGER_PIP,
+            HandLandmark.INDEX_FINGER_DIP,
+            HandLandmark.INDEX_FINGER_DIP,
+            HandLandmark.INDEX_FINGER_TIP,
+            HandLandmark.INDEX_FINGER_MCP,
+            HandLandmark.MIDDLE_FINGER_MCP,
+            HandLandmark.MIDDLE_FINGER_MCP,
+            HandLandmark.MIDDLE_FINGER_PIP,
+            HandLandmark.MIDDLE_FINGER_PIP,
+            HandLandmark.MIDDLE_FINGER_DIP,
+            HandLandmark.MIDDLE_FINGER_DIP,
+            HandLandmark.MIDDLE_FINGER_TIP,
+            HandLandmark.MIDDLE_FINGER_MCP,
+            HandLandmark.RING_FINGER_MCP,
+            HandLandmark.RING_FINGER_MCP,
+            HandLandmark.RING_FINGER_PIP,
+            HandLandmark.RING_FINGER_PIP,
+            HandLandmark.RING_FINGER_DIP,
+            HandLandmark.RING_FINGER_DIP,
+            HandLandmark.RING_FINGER_TIP,
+            HandLandmark.RING_FINGER_MCP,
+            HandLandmark.PINKY_MCP,
+            HandLandmark.WRIST,
+            HandLandmark.PINKY_MCP,
+            HandLandmark.PINKY_MCP,
+            HandLandmark.PINKY_PIP,
+            HandLandmark.PINKY_PIP,
+            HandLandmark.PINKY_DIP,
+            HandLandmark.PINKY_DIP,
+            HandLandmark.PINKY_TIP
+        )
+    }
+}
