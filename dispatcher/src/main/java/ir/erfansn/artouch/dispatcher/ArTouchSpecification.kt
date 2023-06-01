0a5c60caa3be6fb11233fa323cb283b0d3b7b506
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-06-01 18:52:01 +0330

    Add ArTouchSpecification

---
+++ dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/ArTouchSpecification.kt
@@ -0,0 +1,10 @@
+package ir.erfansn.artouch.dispatcher
+
+import android.os.ParcelUuid
+import java.util.UUID
+
+internal object ArTouchSpecification {
+
+    // Formatted in SIG-adopted base UUID 0000xxxx-0000-1000-8000-00805F9B34FB.
+    val SERVICE_UUID = ParcelUuid(UUID.fromString("00004397-0000-1000-8000-00805F9B34FB"))
+}
