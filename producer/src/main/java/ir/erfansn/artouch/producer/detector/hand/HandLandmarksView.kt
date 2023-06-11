635598e37255e0da43e77d3495004b12d64b6c90
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-06-11 21:26:25 +0330

    Refactor the codes of drawing points and lines

--- producer/src/main/java/ir/erfansn/artouch/producer/detector/hand/HandLandmarksView.kt
+++ producer/src/main/java/ir/erfansn/artouch/producer/detector/hand/HandLandmarksView.kt
@@ -7,7 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
-import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmark
+import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmarker
 import kotlin.math.max
 
 class HandLandmarksView(context: Context, attrs: AttributeSet) : View(context, attrs) {
@@ -36,24 +36,26 @@ class HandLandmarksView(context: Context, attrs: AttributeSet) : View(context, a
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
         result?.let { result ->
-            val lines = mutableListOf<Float>()
-            val points = mutableListOf<Float>()
-
             for (landmarks in result.landmarks) {
-                for (i in landmarkConnections.indices step 2) {
-                    val startX = landmarks[landmarkConnections[i]].previewOptimized.x()
-                    val startY = landmarks[landmarkConnections[i]].previewOptimized.y()
-                    val endX = landmarks[landmarkConnections[i + 1]].previewOptimized.x()
-                    val endY = landmarks[landmarkConnections[i + 1]].previewOptimized.y()
-                    lines.add(startX)
-                    lines.add(startY)
-                    lines.add(endX)
-                    lines.add(endY)
-                    points.add(startX)
-                    points.add(startY)
+                for (connection in HandLandmarker.HAND_CONNECTIONS) {
+                    val startPreviewOptimizedLandmark = landmarks[connection.start()].previewOptimized
+                    val endPreviewOptimizedLandmark = landmarks[connection.end()].previewOptimized
+                    canvas.drawLine(
+                        startPreviewOptimizedLandmark.x(),
+                        startPreviewOptimizedLandmark.y(),
+                        endPreviewOptimizedLandmark.x(),
+                        endPreviewOptimizedLandmark.y(),
+                        linePaint
+                    )
                 }
-                canvas.drawLines(lines.toFloatArray(), linePaint)
-                canvas.drawPoints(points.toFloatArray(), pointPaint)
+                for (normalizedLandmark in landmarks) {
+                    val previewOptimizedLandmark = normalizedLandmark.previewOptimized
+                    canvas.drawPoint(
+                        previewOptimizedLandmark.x(),
+                        previewOptimizedLandmark.y(),
+                        pointPaint
+                    )
+                }
             }
         }
     }
@@ -84,53 +86,5 @@ class HandLandmarksView(context: Context, attrs: AttributeSet) : View(context, a
 
     companion object {
         private const val LANDMARK_STROKE_WIDTH = 8F
-
-        // This list defines the lines that are drawn when visualizing the hand landmark detection
-        // results. These lines connect:
-        // landmarkConnections[2*n] and landmarkConnections[2*n+1]
-        private val landmarkConnections = listOf(
-            HandLandmark.WRIST,
-            HandLandmark.THUMB_CMC,
-            HandLandmark.THUMB_CMC,
-            HandLandmark.THUMB_MCP,
-            HandLandmark.THUMB_MCP,
-            HandLandmark.THUMB_IP,
-            HandLandmark.THUMB_IP,
-            HandLandmark.THUMB_TIP,
-            HandLandmark.WRIST,
-            HandLandmark.INDEX_FINGER_MCP,
-            HandLandmark.INDEX_FINGER_MCP,
-            HandLandmark.INDEX_FINGER_PIP,
-            HandLandmark.INDEX_FINGER_PIP,
-            HandLandmark.INDEX_FINGER_DIP,
-            HandLandmark.INDEX_FINGER_DIP,
-            HandLandmark.INDEX_FINGER_TIP,
-            HandLandmark.INDEX_FINGER_MCP,
-            HandLandmark.MIDDLE_FINGER_MCP,
-            HandLandmark.MIDDLE_FINGER_MCP,
-            HandLandmark.MIDDLE_FINGER_PIP,
-            HandLandmark.MIDDLE_FINGER_PIP,
-            HandLandmark.MIDDLE_FINGER_DIP,
-            HandLandmark.MIDDLE_FINGER_DIP,
-            HandLandmark.MIDDLE_FINGER_TIP,
-            HandLandmark.MIDDLE_FINGER_MCP,
-            HandLandmark.RING_FINGER_MCP,
-            HandLandmark.RING_FINGER_MCP,
-            HandLandmark.RING_FINGER_PIP,
-            HandLandmark.RING_FINGER_PIP,
-            HandLandmark.RING_FINGER_DIP,
-            HandLandmark.RING_FINGER_DIP,
-            HandLandmark.RING_FINGER_TIP,
-            HandLandmark.RING_FINGER_MCP,
-            HandLandmark.PINKY_MCP,
-            HandLandmark.WRIST,
-            HandLandmark.PINKY_MCP,
-            HandLandmark.PINKY_MCP,
-            HandLandmark.PINKY_PIP,
-            HandLandmark.PINKY_PIP,
-            HandLandmark.PINKY_DIP,
-            HandLandmark.PINKY_DIP,
-            HandLandmark.PINKY_TIP
-        )
     }
 }
