a0be3ae49dd7b370ccaa49c02b0b81ba27476415
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-06-23 23:51:23 +0330

    Change the device name when starting ad

--- dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/ble/peripheral/advertiser/ArTouchPeripheralAdvertiser.kt
+++ dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/ble/peripheral/advertiser/ArTouchPeripheralAdvertiser.kt
@@ -1,10 +1,12 @@
 package ir.erfansn.artouch.dispatcher.ble.peripheral.advertiser
 
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.AdvertiseCallback
 import android.bluetooth.le.AdvertiseData
 import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.Context
 import android.util.Log
 import androidx.core.content.getSystemService
@@ -27,8 +29,8 @@ class ArTouchPeripheralAdvertiser(context: Context) : BleHidPeripheralAdvertiser
     )
 
     private val bluetoothManager = context.getSystemService<BluetoothManager>()!!
-    private val bluetoothAdapter = bluetoothManager.adapter
-    private val bleAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
+    private val bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
+    private val bleAdvertiser: BluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
 
     private val advertiseCallback = object : AdvertiseCallback() {
         override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
@@ -42,12 +44,9 @@ class ArTouchPeripheralAdvertiser(context: Context) : BleHidPeripheralAdvertiser
         }
     }
 
-    init {
-        bluetoothAdapter.name = ArTouchSpecification.NAME
-    }
-
     override fun startAdvertising() {
         scope.launch {
+            bluetoothAdapter.name = ArTouchSpecification.NAME
             arTouchPeripheralManager.registerDevice()
             bleAdvertiser.startAdvertising(
                 AdvertiseSettings.Builder()
@@ -68,6 +67,7 @@ class ArTouchPeripheralAdvertiser(context: Context) : BleHidPeripheralAdvertiser
     override fun stopAdvertising() {
         bleAdvertiser.stopAdvertising(advertiseCallback)
         arTouchPeripheralManager.unregisterDevice()
+        Log.i(TAG, "Advertising stopped")
     }
 
     override fun onDestroy(owner: LifecycleOwner) {
