16ac16b4a290a04566ae8a24ad92cb61995dec21
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-06-26 23:36:50 +0330

    Refactor codes

--- app/src/main/java/ir/erfansn/artouch/MainActivity.kt
+++ app/src/main/java/ir/erfansn/artouch/MainActivity.kt
@@ -3,8 +3,6 @@ package ir.erfansn.artouch
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import ir.erfansn.artouch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,18 +10,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setUpImmersiveMode()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    private fun setUpImmersiveMode() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        WindowCompat.getInsetsController(window, window.decorView).also {
-            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            it.hide(WindowInsetsCompat.Type.systemBars())
-        }
     }
 }
