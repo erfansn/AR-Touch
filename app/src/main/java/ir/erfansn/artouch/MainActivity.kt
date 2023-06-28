e011b917e7d1ed6d61e0e4c1ec88e7a163224896
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-06-28 22:47:10 +0330

    Replace ActivityMainBinding with layout id

--- app/src/main/java/ir/erfansn/artouch/MainActivity.kt
+++ app/src/main/java/ir/erfansn/artouch/MainActivity.kt
@@ -3,17 +3,12 @@ package ir.erfansn.artouch
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import ir.erfansn.artouch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
