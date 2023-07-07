b290510743f76e2ed5f794149e6484f2151fa49a
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-07-07 19:07:04 +0330

    Implement BluetoothHelper

---
+++ dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/DefaultBluetoothHelper.kt
@@ -0,0 +1,47 @@
+package ir.erfansn.artouch.dispatcher
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+
+class DefaultBluetoothHelper(private val context: Context) : BluetoothHelper {
+
+    private val bluetoothManager = context.getSystemService<BluetoothManager>()!!
+    private val bluetoothAdapter = bluetoothManager.adapter
+
+    override val enabled get() = bluetoothAdapter.isEnabled
+
+    override val allPermissionsGranted
+        get() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            true
+        } else {
+            BLUETOOTH_PERMISSIONS.all {
+                ContextCompat.checkSelfPermission(
+                    context,
+                    it
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+        }
+
+    @SuppressLint("MissingPermission")
+    override val bondedDevices = flow {
+        while (true) {
+            emit(runCatching { bluetoothAdapter.bondedDevices }.getOrDefault(emptySet()).toList())
+            delay(1000)
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+private val BLUETOOTH_PERMISSIONS = arrayOf(
+    Manifest.permission.BLUETOOTH_ADVERTISE,
+    Manifest.permission.BLUETOOTH_CONNECT,
+)
