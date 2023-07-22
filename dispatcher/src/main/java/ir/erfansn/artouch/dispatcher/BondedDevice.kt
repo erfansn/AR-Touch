95ff9b4b6a195fa2fdea24a8a971a4465c89f233
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-07-22 21:01:11 +0330

    Parcelize BondedDevice

--- dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/BondedDevice.kt
+++ dispatcher/src/main/java/ir/erfansn/artouch/dispatcher/BondedDevice.kt
@@ -2,10 +2,13 @@ package ir.erfansn.artouch.dispatcher
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 @SuppressLint("MissingPermission")
+@Parcelize
 data class BondedDevice(
     val device: BluetoothDevice? = null,
     val name: String = device?.name.orEmpty(),
     val address: String = device?.address.orEmpty(),
-)
+) : Parcelable
