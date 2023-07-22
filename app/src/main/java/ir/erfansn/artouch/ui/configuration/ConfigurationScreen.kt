4918d1e1d575060ffc2e0eee621324ae39be8646
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-07-22 19:51:45 +0330

    Add test tag to LazyColumn

--- app/src/main/java/ir/erfansn/artouch/ui/configuration/ConfigurationScreen.kt
+++ app/src/main/java/ir/erfansn/artouch/ui/configuration/ConfigurationScreen.kt
@@ -52,6 +52,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -191,7 +192,7 @@ fun ConfigurationScreen(
                                     text = stringResource(R.string.bonded_devices_list_is_empty)
                                 )
                             } else {
-                                LazyColumn {
+                                LazyColumn(modifier = Modifier.testTag("bonded_devices_list")) {
                                     items(bluetoothBondedDevices) {
                                         ListItem(
                                             modifier = Modifier
