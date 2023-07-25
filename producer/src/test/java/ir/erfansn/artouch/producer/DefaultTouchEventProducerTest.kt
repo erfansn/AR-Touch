a7de45baf673b1672225180490750a8bd80b1d3e
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2023-07-25 18:34:42 +0330

    Improve readability of tests

--- producer/src/test/java/ir/erfansn/artouch/producer/DefaultTouchEventProducerTest.kt
+++ producer/src/test/java/ir/erfansn/artouch/producer/DefaultTouchEventProducerTest.kt
@@ -2,11 +2,11 @@ package ir.erfansn.artouch.producer
 
 import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
 import com.google.mediapipe.tasks.vision.handlandmarker.HandLandmark
-import ir.erfansn.artouch.producer.detector.hand.HandDetectionResult
-import ir.erfansn.artouch.producer.detector.aruco.ArUcoDetectionResult
-import ir.erfansn.artouch.producer.extractor.TouchPositionExtractor
 import ir.erfansn.artouch.common.util.Point
 import ir.erfansn.artouch.common.util.Size
+import ir.erfansn.artouch.producer.detector.aruco.ArUcoDetectionResult
+import ir.erfansn.artouch.producer.detector.hand.HandDetectionResult
+import ir.erfansn.artouch.producer.extractor.StubTouchPositionExtractor
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
@@ -42,14 +42,10 @@ class DefaultTouchEventProducerTest {
 
     @Before
     fun setUp() {
-        val stubTouchPositionExtractor = object : TouchPositionExtractor {
-            override fun extract(target: Point, boundary: Array<Point>) =
-                Point(-1f, -1f)
-        }
         touchEventProducer = DefaultTouchEventProducer(
             handDetectionResult = handDetectionResult,
             arUcoDetectionResult = arucoDetectionResult,
-            touchPositionExtractor = stubTouchPositionExtractor,
+            touchPositionExtractor = StubTouchPositionExtractor,
             defaultDispatcher = testDispatcher,
         )
     }
@@ -99,45 +95,12 @@ class DefaultTouchEventProducerTest {
     }
 
     @Test
-    fun producesReleasedEvent_whenFingersNotCloseEnough() = runTest(testDispatcher) {
-        handDetectionResult.update {
-            it.copy(
-                landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.05f,
-                    touchFingersAngleDegrees = 18.0
-                )
-            )
-        }
-
-        val touchEvent = touchEventProducer.touchEvent.first()
-
-        assertFalse(touchEvent.pressed)
-    }
-
-    @Test
-    fun producesPressedEvent_whenFingersClosedEnough() = runTest(testDispatcher) {
-        handDetectionResult.update {
-            it.copy(
-                landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.031f,
-                    touchFingersAngleDegrees = 10.0,
-                )
-            )
-        }
-
-        val touchEvent = touchEventProducer.touchEvent.first()
-
-        assertTrue(touchEvent.pressed)
-    }
-
-    @Test
     fun producesReleasedEvent_whenFingersNotClosedEnoughInsideBigFrame() = runTest(testDispatcher) {
         handDetectionResult.update {
             it.copy(
                 landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.003f,
-                    touchFingersAngleDegrees = 18.0,
-                    inFarDistance = true,
+                    hasSmallHand = true,
+                    fingersClosed = false,
                 )
             )
         }
@@ -152,9 +115,8 @@ class DefaultTouchEventProducerTest {
         handDetectionResult.update {
             it.copy(
                 landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.003f,
-                    touchFingersAngleDegrees = 10.0,
-                    inFarDistance = true
+                    hasSmallHand = true,
+                    fingersClosed = true,
                 )
             )
         }
@@ -165,30 +127,29 @@ class DefaultTouchEventProducerTest {
     }
 
     @Test
-    fun producesReleasedEvent_whenFingersNotClosedEnoughInsideSmallFrame() = runTest(testDispatcher) {
-        handDetectionResult.update {
-            it.copy(
-                landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.05f,
-                    touchFingersAngleDegrees = 20.0,
-                    inFarDistance = false
+    fun producesReleasedEvent_whenFingersNotClosedEnoughInsideSmallFrame() =
+        runTest(testDispatcher) {
+            handDetectionResult.update {
+                it.copy(
+                    landmarks = createFakeHandLandmarks(
+                        hasSmallHand = false,
+                        fingersClosed = false,
+                    )
                 )
-            )
+            }
+
+            val touchEvent = touchEventProducer.touchEvent.first()
+
+            assertFalse(touchEvent.pressed)
         }
-
-        val touchEvent = touchEventProducer.touchEvent.first()
-
-        assertFalse(touchEvent.pressed)
-    }
 
     @Test
     fun producesPressedEvent_whenFingersClosedEnoughInsideSmallFrame() = runTest(testDispatcher) {
         handDetectionResult.update {
             it.copy(
                 landmarks = createFakeHandLandmarks(
-                    touchFingersLength = 0.05f,
-                    touchFingersAngleDegrees = 9.0,
-                    inFarDistance = false
+                    hasSmallHand = false,
+                    fingersClosed = true,
                 )
             )
         }
@@ -199,12 +160,18 @@ class DefaultTouchEventProducerTest {
     }
 
     private fun createFakeHandLandmarks(
-        touchFingersLength: Float,
-        touchFingersAngleDegrees: Double,
-        inFarDistance: Boolean? = null,
+        hasSmallHand: Boolean,
+        fingersClosed: Boolean,
     ): List<NormalizedLandmark> {
-        val randomPoint =
-            Point(Random.nextFloat(), Random.nextFloat())
+        val inFarDistance: Boolean = hasSmallHand
+        val (touchFingersLength, touchFingersAngleDegrees) = when {
+            hasSmallHand && fingersClosed -> 0.0001f to 1.0
+            hasSmallHand && !fingersClosed -> 0.0001f to 40.0
+            !hasSmallHand && fingersClosed -> 0.1f to 1.0
+            else -> 0.1f to 40.0
+        }
+
+        val randomPoint = Point(Random.nextFloat(), Random.nextFloat())
         val otherPoint = randomPoint + Point(touchFingersLength, 0f)
 
         val vertexLength = touchFingersLength / tan(Math.toRadians(touchFingersAngleDegrees))
@@ -212,24 +179,27 @@ class DefaultTouchEventProducerTest {
 
         val handLandmarks = mutableListOf<NormalizedLandmark>()
         repeat(HandLandmark.NUM_LANDMARKS) {
-            handLandmarks += when(it) {
+            handLandmarks += when (it) {
                 HandLandmark.INDEX_FINGER_TIP -> {
                     NormalizedLandmark.create(randomPoint.x, randomPoint.y, 0f)
                 }
+
                 HandLandmark.MIDDLE_FINGER_TIP -> {
                     NormalizedLandmark.create(otherPoint.x, otherPoint.y, 0f)
                 }
+
                 HandLandmark.INDEX_FINGER_MCP -> {
-                    NormalizedLandmark.create(vertex.x + 0.02f, vertex.y, 0f)
+                    NormalizedLandmark.create(vertex.x + 0.002f, vertex.y, 0f)
                 }
+
                 HandLandmark.MIDDLE_FINGER_MCP -> {
-                    NormalizedLandmark.create(vertex.x - 0.02f, vertex.y, 0f)
+                    NormalizedLandmark.create(vertex.x - 0.002f, vertex.y, 0f)
                 }
+
                 HandLandmark.WRIST -> {
-                    NormalizedLandmark.create(0f, 0f, inFarDistance?.let {
-                        if (it) 0f else 1f
-                    } ?: Random.nextFloat())
+                    NormalizedLandmark.create(0f, 0f, if (inFarDistance) 0f else 1f)
                 }
+
                 else -> {
                     NormalizedLandmark.create(0f, 0f, 0f)
                 }
