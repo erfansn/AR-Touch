a5c738f697915e75a38607a112d361c7e951346d
Erfan Sn <50053121+ErfanSn@users.noreply.github.com>
2024-06-24 18:08:49 +0330

    Remove Androidx Test Orchestrator

--- benchmark/src/androidTest/java/ir/erfansn/artouch/benchmark/ArUcoDetectorBenchmark.kt
+++ benchmark/src/androidTest/java/ir/erfansn/artouch/benchmark/ArUcoDetectorBenchmark.kt
@@ -16,30 +16,21 @@
 
 package ir.erfansn.artouch.benchmark
 
-import android.content.Context
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
-import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
 import ir.erfansn.artouch.benchmark.di.testModule
 import ir.erfansn.artouch.benchmark.util.loadJpegImageIntoImageProxy
 import ir.erfansn.artouch.producer.detector.ObjectDetector
 import ir.erfansn.artouch.producer.detector.aruco.ArUcoDetectionResult
 import ir.erfansn.artouch.producer.di.ARUCO_DETECTOR_QUALIFIER
 import ir.erfansn.artouch.producer.di.producerModule
-import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runners.MethodSorters
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.get
 
-// If you have a Xiaomi device and cannot run the tests, follow the steps below:
-//  1. Click gutter icon to run whole tests
-//  2. When running a_fakeTest enter follow command in terminal
-//  "adb shell am start -n 'ir.erfansn.artouch.benchmark.test/androidx.test.core.app.InstrumentationActivityInvoker\$BootstrapActivity'"
-//  3. Wait to complete tests
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ArUcoDetectorBenchmark : KoinTest {
 
     @get:Rule(order = 0)
@@ -51,12 +42,9 @@ class ArUcoDetectorBenchmark : KoinTest {
     val benchmarkRule = BenchmarkRule()
 
     @Test
-    fun a_fakeTest() = Unit
-
-    @Test
     fun detectingBenchmark() {
         val arUcoMarkerDetector = get<ObjectDetector<ArUcoDetectionResult>>(ARUCO_DETECTOR_QUALIFIER)
-        val context = ApplicationProvider.getApplicationContext<Context>()
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
         val arucoImage = context.loadJpegImageIntoImageProxy("aruco.jpg")
 
         benchmarkRule.measureRepeated {
--- benchmark/src/androidTest/java/ir/erfansn/artouch/benchmark/HandDetectorBenchmark.kt
+++ benchmark/src/androidTest/java/ir/erfansn/artouch/benchmark/HandDetectorBenchmark.kt
@@ -16,33 +16,26 @@
 
 package ir.erfansn.artouch.benchmark
 
-import android.content.Context
 import android.graphics.PixelFormat
 import androidx.benchmark.junit4.BenchmarkRule
-import androidx.test.core.app.ApplicationProvider
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.platform.app.InstrumentationRegistry
 import ir.erfansn.artouch.benchmark.di.testModule
 import ir.erfansn.artouch.benchmark.util.loadJpegImageIntoImageProxy
 import ir.erfansn.artouch.producer.detector.ObjectDetector
 import ir.erfansn.artouch.producer.detector.hand.HandDetectionResult
 import ir.erfansn.artouch.producer.di.HAND_DETECTOR_QUALIFIER
 import ir.erfansn.artouch.producer.di.producerModule
-import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runners.MethodSorters
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.get
 
-// If you have a Xiaomi device and cannot run the tests, follow the steps below:
-//  1. Click gutter icon to run whole tests
-//  2. When running a_fakeTest enter follow command in terminal
-//  "adb shell am start -n 'ir.erfansn.artouch.benchmark.test/androidx.test.core.app.InstrumentationActivityInvoker\$BootstrapActivity'"
-//  3. Wait to complete tests
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class HandDetectorBenchmark : KoinTest {
 
     @get:Rule(order = 0)
@@ -54,30 +47,19 @@ class HandDetectorBenchmark : KoinTest {
     val benchmarkRule = BenchmarkRule()
 
     @Test
-    fun a_fakeTest() = Unit
-
-    @Test
-    fun detectingBenchmark() = runBlocking {
+    fun detectingBenchmark() {
         val handLandmarkerDetector = get<ObjectDetector<HandDetectionResult>>(HAND_DETECTOR_QUALIFIER)
-        val context = ApplicationProvider.getApplicationContext<Context>()
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
         val handImage = context.loadJpegImageIntoImageProxy("hand.jpg", format = PixelFormat.RGBA_8888)
 
         benchmarkRule.measureRepeated {
-            val result = async { handLandmarkerDetector.result.first() }
-            handLandmarkerDetector.detect(handImage)
-            result.await()
+            runBlocking {
+                val result = launch { handLandmarkerDetector.result.first() }
+                runWithTimingDisabled { delay(1000) }
+                handLandmarkerDetector.detect(handImage)
+                result.join()
+            }
         }
     }
 }
 
-private inline fun BenchmarkRule.measureRepeated(block: BenchmarkRule.Scope.() -> Unit) {
-    // Note: this is an extension function to discourage calling from Java.
-
-    // Extract members to locals, to ensure we check #applied, and we don't hit accessors
-    val localState = getState()
-    val localScope = scope
-
-    while (localState.keepRunningInline()) {
-        block(localScope)
-    }
-}
